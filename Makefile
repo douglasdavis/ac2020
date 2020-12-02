714ebd7e2b958fe894c54006697afff72dd8a298
Doug Davis <ddavis@ddavis.io>
2020-12-02 12:12:43 -0500

    day2

--- Makefile
+++ Makefile
@@ -1,11 +1,13 @@
+all: day1 day2
+	-@echo "Done"
+
 day1:
 	cc day1.c -o day1
 
-
-all:
-	day1
+day2:
+	cc day2.c -o day2
 
 .PHONY: clean
 
 clean:
-	rm -f day1
+	rm -f day1 day2
---
+++ day2.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct {
+  int min;
+  int max;
+  char c;
+  char* s;
+} entry_t;
+
+int is_valid_part_one(const entry_t* entry) {
+  int ccount = 0;
+  char* s = entry->s;
+  for (size_t i = 0; i < strlen(s); ++i) {
+    if (s[i] == entry->c) {
+      ccount++;
+    }
+  }
+  if (ccount >= entry->min && ccount <= entry->max) {
+    return 1;
+  }
+  return 0;
+}
+
+int is_valid_part_two(const entry_t* entry) {
+  char* s = entry->s;
+  int t1 = s[entry->min - 1] == entry->c;
+  int t2 = s[entry->max - 1] == entry->c;
+  return (t1 + t2) == 1;
+}
+
+void print_entry(const entry_t* entry) {
+  printf("%d-%d %c: %s\n", entry->min, entry->max, entry->c, entry->s);
+}
+
+int main(int argc, char *argv[]) {
+  int total_good1 = 0;
+  int total_good2 = 0;
+  FILE* f;
+  f = fopen("day2.dat", "r");
+  int i = 0;
+  int min;
+  int max;
+  char c;
+  char s[50];
+  while (fscanf(f, "%d-%d %c: %s", &min, &max, &c, s) == 4) {
+    entry_t entry;
+    entry.min = min;
+    entry.max = max;
+    entry.c = c;
+    entry.s = s;
+    total_good1 += is_valid_part_one(&entry);
+    total_good2 += is_valid_part_two(&entry);
+  }
+  fclose(f);
+  printf("%d\n", total_good1);
+  printf("%d\n", total_good2);
+  return 0;
+}

05bd6e78bd0abe6a3ec4ccaba84124e19a180fb6
Doug Davis <ddavis@ddavis.io>
2020-12-02 11:05:14 -0500

    first

---
+++ Makefile
@@ -0,0 +1,11 @@
+day1:
+	cc day1.c -o day1
+
+
+all:
+	day1
+
+.PHONY: clean
+
+clean:
+	rm -f day1
---
+++ day1.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+#define N 200
+
+int main(int argc, char *argv[]) {
+
+  /* place to store ints */
+  int all[N] = {0};
+
+  /* read all the ints */
+  FILE* f;
+  int value;
+  f = fopen("day1.dat", "r");
+  int i = 0;
+  while (fscanf(f, "%d", &value) == 1) {
+    all[i] = value;
+    i++;
+  }
+  fclose(f);
+
+  /* brute force find the first answer */
+  int answer1 = 0;
+  for (int j = 0; j < N; ++j) {
+    for (int k = 1; k < N; ++k) {
+      if ((all[j] + all[k]) == 2020) {
+        answer1 = all[j] * all[k];
+        break;
+      }
+    }
+  }
+
+  /* brute force find the answer to part 2 */
+  int answer2 = 0;
+  for (int j = 0; j < N; ++j) {
+    for (int k = 1; k < N; ++k) {
+      for (int m = 2; m < N; ++m) {
+        if ((all[j] + all[k] + all[m]) == 2020) {
+          answer2 = all[j] * all[k] * all[m];
+          break;
+        }
+      }
+    }
+  }
+
+  /* report the answers */
+  printf("%d\n", answer1);
+  printf("%d\n", answer2);
+  return 0;
+}

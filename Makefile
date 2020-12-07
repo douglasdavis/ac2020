2b21296cf6b593dc9c6f45a8efac176d093e7370
Doug Davis <ddavis@ddavis.io>
2020-12-07 09:43:47 -0500

    day3

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: day1 day2
+all: day1 day2 day3
 	-@echo "Done"
 
 day1: day1.c
@@ -7,7 +7,10 @@ day1: day1.c
 day2: day2.c
 	cc day2.c -o day2
 
-.PHONY: clean
+day3: day3.c
+	cc day3.c -o day3
+
+.PHONY: clean all
 
 clean:
-	rm -f day1 day2
+	rm -f day1 day2 day3
---
+++ day3.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define N 31
+#define TREE  '#'
+
+int main(int argc, char *argv[]) {
+  FILE* f;
+  f = fopen("day3.dat", "r");
+  int irow = 0;
+  int ntree = 0;
+  char in_row[N];
+  int overall_cell = 0;
+  int current_cell = 0;
+
+  int n_right = atoi(argv[1]);
+  printf("%d\n", n_right);
+
+  while (fscanf(f, "%s", in_row) == 1) {
+    irow++;
+    /* uncomment for part 2 */
+    /* if (irow%2 != 0) { */
+    if (1) {
+      if (overall_cell > 30) {
+        current_cell = (overall_cell % 31);
+      }
+      else {
+        current_cell = overall_cell;
+      }
+      char* row = in_row;
+      if (row[current_cell] == TREE) {
+        ntree++;
+      }
+      overall_cell += n_right;
+    }
+  }
+
+  printf("%d\n", ntree);
+
+  fclose(f);
+  return 0;
+}
